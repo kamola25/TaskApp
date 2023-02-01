e0c09de19a81910c88267b99c0bf388f9c825c54
kamola25 <kamolaboribaevak13@gmail.com>
2023-02-01 19:23:19 +0300

    add logic for switcher mode

--- app/src/main/java/com/kiro/taskapp/MainFragment.kt
+++ app/src/main/java/com/kiro/taskapp/MainFragment.kt
@@ -1,32 +1,50 @@
 package com.kiro.taskapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import com.kiro.taskapp.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    private lateinit var binding : FragmentMainBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val navController =
             requireActivity().supportFragmentManager.findFragmentById(R.id.navHost_fragment) as NavHostFragment
-        val button = view.findViewById<Button>(R.id.btn_edit)
-        button.setOnClickListener {
+        binding.btnEdit.setOnClickListener {
             navController.navController.navigate(R.id.editFragment)
         }
+
+
+        binding.switcherMode.setOnClickListener {
+            if (binding.switcherMode.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.switcherMode.text = "Disable dark mode"
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.switcherMode.text = "Enable dark mode"
+            }
+        }
+
+
+
     }
 
 }
